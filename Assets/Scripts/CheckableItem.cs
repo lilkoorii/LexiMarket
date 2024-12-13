ed406184cdbe1c9d98a96c1ac02c5c03a7e8f9f4
Porkothh <112623227+Porkothh@users.noreply.github.com>
2024-12-13 10:37:12 +0300

    zone checker

---
+++ Assets/Scripts/CheckableItem.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckableItem : MonoBehaviour
+{
+    public Item item;
+    private ItemRequester itemRequester;
+
+    void Start()
+    {
+        itemRequester = FindObjectOfType<ItemRequester>();
+        if (itemRequester == null)
+        {
+            Debug.LogError("ItemRequester not found in the scene!");
+        }
+    }
+
+    public bool CheckMatch()
+    {
+        if (itemRequester == null || item == null)
+            return false;
+            
+        return item == itemRequester.RequestedItem;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    transform.Translate(Vector3.forward * Time.deltaTime);
+        
+    }
+}
---
+++ Assets/Scripts/RequestedItemZone.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RequestedItemZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // Проверяем, есть ли у объекта компонент CheckableItem
+        CheckableItem checkableItem = other.GetComponent<CheckableItem>();
+        
+        if (checkableItem != null)
+        {
+            // Проверяем, совпадает ли предмет с запрошенным
+            if (checkableItem.CheckMatch())
+            {
+                Debug.Log("Correct item placed!");
+                // Здесь можно добавить логику для правильного предмета
+                // Например, начислить очки или показать эффект
+            }
+            else
+            {
+                Debug.Log("Wrong item placed!");
+                // Здесь можно добавить логику для неправильного предмета
+                // Например, показать сообщение об ошибке
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        CheckableItem checkableItem = other.GetComponent<CheckableItem>();
+        if (checkableItem != null)
+        {
+            // Здесь можно добавить логику при удалении предмета из зоны
+            Debug.Log("Item removed from zone");
+        }
+    }
+}
